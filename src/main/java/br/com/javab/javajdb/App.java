a635796f6db92ed5b2b3fb4ed66ee876fbd8cb2b
joaovslins <86032378+joaovslins@users.noreply.github.com>
2021-06-17 16:56:07 -0300

    Add files via upload

---
+++ src/main/java/br/com/javab/javajdb/App.java
@@ -0,0 +1,39 @@
+package br.com.javab.javajdb;
+import java.sql.*;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	/*Conexão com o banco Mysql*/
+    	/*
+    	try{
+    	Connection conec = DriverManager.getConnection("jdbc:mysql://host_do_banco/nome_do_banco?serverTimezone=America/Sao_Paulo", "usuário", "senha");
+    	
+    		conec.close();
+    	
+    	}catch(SQLException erro){
+    		
+    		erro.printStackTrace();
+    	}
+    	*/
+    	/*Conexão com o banco Postgresql*/
+    	/*
+    	try{
+    	
+    		Connection conec = DriverManager.getConnection("jdbc:postgresql://host_do_banco/nome_do_banco?serverTimezone=America/Sao_Paulo", "usuário", "senha");
+    	
+		conec.close();
+	
+    	}catch(SQLException erro){
+		
+    		erro.printStackTrace();
+    	}
+    	*/
+    	
+    	
+    	
+    	
+    }
+}
